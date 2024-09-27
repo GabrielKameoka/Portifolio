5b419d7de15ba9e7eb45891a5ef503876ab1b8b9
GabrielKameoka <gabrielkameoka@gmail.com>
2024-09-27 13:36:00 -0300

    finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', () => {
+    const techSection = document.getElementById('tech');
+    const techSectionTop = techSection.offsetTop;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
+  
+    if (scrollPosition >= techSectionTop - windowHeight + 700) {
+      techSection.classList.add('show');
+    } else {
+      techSection.classList.remove('show');
+    }
+  });
+  
+  window.addEventListener('scroll', () => {
+    const conteudoP = document.querySelector('.conteudo-p'); // Corrigido: use querySelector
+    const conteudoPTop = conteudoP.offsetTop;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
+  
+    if (scrollPosition >= conteudoPTop - windowHeight + 780 ) {
+      conteudoP.classList.add('show');
+    } else {
+      conteudoP.classList.remove('show');
+    }
+  });
