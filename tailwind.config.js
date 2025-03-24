83e57adf9fa0b4e9084cf2c09b7bf63e452aa9a1
GabrielKameoka <gabrielkameoka@gmail.com>
2025-03-24 17:27:03 -0300

    html finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], // Adicionando Montserrat
+      },
+    },
+  },
+};
+
+  
